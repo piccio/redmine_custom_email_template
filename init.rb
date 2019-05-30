5d858a710ae2606800ad54c29f967d6193bfaed1
Roberto Piccini <roberto.piccini@gmail.com>
2019-05-30 16:35:20 +0200

    swap description to comment

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :redmine_custom_email_template do
   name 'Redmine Custom Email Template plugin'
   author 'Roberto Piccini'
   description 'override email views'
-  version '1.0.0'
+  version '1.1.1'
   url 'https://github.com/piccio/redmine_custom_email_template'
   author_url 'https://github.com/piccio'
 end
